1c94cf644df592593e2491b8109cf0f7afbb3bdc
arya.julianda <arya.julianda@alpabit.com>
2026-03-26 01:53:23 +0700

    refactor: update welcome page content and comment out registration link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,7 +44,7 @@
             </li>
           @else
             <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Login</a></li>
-            <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">Register</a></li>
+            {{-- <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">Register</a></li> --}}
           @endauth
         </ul>
       </div>
