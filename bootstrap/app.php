97b4c12a5f2355275147e36d0eaad9416ff8ec60
arya.julianda <arya.julianda@alpabit.com>
2026-04-12 01:36:48 +0700

    feat: add trust proxies middleware configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 use App\Http\Middleware\AdminRole;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -12,6 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_ALL
+        );
         $middleware->alias([
             'admin' => AdminRole::class,
         ]);
